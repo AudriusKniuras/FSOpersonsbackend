500431620caa00a4fb8188119b245b27fee0d4ce
Audrius Kniuras <audrius.kniuras@tesonet.com>
2021-12-18 14:12:21 +0200

    fix frontend to show up

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 
-
-app.use(cors())
+app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 
-morgan.token('body', function getBody(req) {
-  if (req.method === 'POST') {
-    return JSON.stringify(req.body)
+morgan.token("body", function getBody(req) {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body);
   }
-  return ''
-})
+  return "";
+});
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body',))
-
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
@@ -103,7 +104,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Server is running on port ", PORT);
 });
